f6d160b397a9dfcae1e5d5f4ca09796bfafe5204
obaid <obaidurrahmanobaid@gmail.com>
2023-04-30 11:21:59 +0530

    Click RecyclerView

--- app/src/main/java/com/example/recyclerview2/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview2/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.example.recyclerview2
 
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.recyclerview2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var binding : ActivityMainBinding? = null
-//    private val binding get() = _binding!!
-
+//    private var binding : ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(R.layout.activity_main)
 
 //        val ItemList : List<String> = listOf("aaaa", "bbbb", "cccc", "dddd", "eeeee" , "ffff", "ggggg")
 //        val mainList = findViewById<RecyclerView>(R.id.itemList)
@@ -24,25 +20,27 @@ class MainActivity : AppCompatActivity() {
 //        mainList.layoutManager = LinearLayoutManager(this)
 
         val MainList = findViewById<RecyclerView>(R.id.itemList)
-        MainList.adapter = FinalAdapter(listOf
-            (
-            Info("User 8" , "You may have a new message"),
-            Info("User 7", "You may have a new message"),
-            Info("User 1", "You may have a new message"),
+        MainList.layoutManager = LinearLayoutManager(this)
+
+        val newVal = listOf<Info>(
+            Info("User 1" , "You may have a new message"),
             Info("User 2", "You may have a new message"),
             Info("User 3", "You may have a new message"),
             Info("User 4", "You may have a new message"),
             Info("User 5", "You may have a new message"),
             Info("User 6", "You may have a new message"),
+            Info("User 7", "You may have a new message"),
+            Info("User 8", "You may have a new message"),
             Info("User 9", "You may have a new message"),
             Info("User 10", "You may have a new message"),
             Info("User 11", "You may have a new message")
-        ), listener = {
-
+        )
+        MainList.adapter = FinalAdapter( newVal, listener = {
+            newVal.get(it).Title
+            Toast.makeText(this, "${newVal.get(it).Title} Clicked", Toast.LENGTH_SHORT).show()
         })
-        MainList.layoutManager = LinearLayoutManager(this)
-    }
 
+    }
 
   class Info(
         val Title : String = "",
